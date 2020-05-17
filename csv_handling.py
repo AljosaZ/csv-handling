30be0a406530f5da8d5c15c2566f0391c7f56a78
AljosaZ <znidaric.aljosa@gmail.com>
2020-05-17 13:19:34 +0200

    skipped first row of csv with enumerate

--- csv_handling.py
+++ csv_handling.py
@@ -3,6 +3,6 @@ import csv
 
 with open('text.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    next(csv_reader)
-    for row in csv_reader:
+    for count, row in enumerate(csv_reader):
+        if count > 0:
             print(f'{row[0]} is {row[2]} and {row[1]} years old.')
